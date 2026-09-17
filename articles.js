dc0944b191ece6bf9aaa3336f5765d99f6255b80
Louisye-analyse <yembikadryl@gmail.com>
2026-09-17 12:37:47 +0200

    Update articles.js

--- articles.js
+++ articles.js
@@ -40,4 +40,10 @@ const ARTICLES = [
     fichier: "articles/2025-05-16-Decoupage-electoral.pdf",
     resume: "Déconnecté des réalités démographiques et géographiques, le découpage électoral gabonais crée des inégalités de représentation. Cette analyse propose deux réformes : un redécoupage fondé sur des critères objectifs ou une pondération des voix parlementaires."
   },
+    {
+    titre: "La procédure de régularisation foncière : Un long chemin semé d'embûches, et un frein à l'élargissement de l'assiette fiscale",
+    date: "2025-05-20",
+    fichier: "articles/2025-05-20-Regularisation-fonciere.pdf",
+    resume: "Au Gabon, la lenteur, l’opacité et le coût des procédures foncières freinent l’accès à la propriété sécurisée, alimentent les conflits et limitent les recettes fiscales. Malgré les ambitions affichées, une réforme structurelle de la chaîne foncière reste attendue."
+  },
 ];
